179e18fdb5cd0553703f24b5371c8f948e7047c3
lubyantsev <162503570+lubyantsev@users.noreply.github.com>
2024-09-03 13:45:05 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,10 +2,10 @@ import logging
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
-API_TOKEN = '7496069476:AAEmEJvx70OUkElm_JV8q4BypnswN3Nk0aA'  # Замените на ваш токен
+API_TOKEN = 'tkn'  # Замените на ваш токен
 
 logging.basicConfig(level=logging.INFO)
 
-bot = Bot(token='7496069476:AAEmEJvx70OUkElm_JV8q4BypnswN3Nk0aA')
+bot = Bot(token='tkn')
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
